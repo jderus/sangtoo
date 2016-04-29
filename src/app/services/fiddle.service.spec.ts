52027b21587d3178a3b99cca42f40b0ffe62faee
jayd <jayd@microsoft.com>
2016-04-28 18:14:06 -0700

    More TDD POC port

--- src/app/services/fiddle.service.spec.ts
+++ src/app/services/fiddle.service.spec.ts
@@ -1,8 +1,20 @@
 import {FiddleService} from './fiddle.service';
 
 describe('fiddle.service', () => {
+  let fiddle: FiddleService;
+  
+  beforeEach(()=>{
+    fiddle = new FiddleService(); 
+  });
+  
   it('true is true', () => expect(true).toEqual(true));
-  it('null is not the same thing as undefined',
-  () => expect(null).not.toEqual(undefined)
-);
+  
+  it('should inject the service',()=> {
+        expect(fiddle).toBeDefined();
+  });
+  
+  it('should have the getData call', function () {
+        expect(fiddle.getData).toBeDefined();
+  });
+  
 });
