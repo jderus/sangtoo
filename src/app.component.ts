fb4791bc3b04494a000e63fab347b3ca172febed
jayd <jayd@microsoft.com>
2016-05-01 18:49:14 -0700

    mdir 4 dep

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,8 +3,10 @@ import {ToyComponent} from './app/components/toy.component';
 
 @Component({
     selector: 'sang',
+    directives:  [ToyComponent],
     template: `<h1>Sangtoo is Alive!</h1>
                <sang-toy></sang-toy>
               `
+              
 })
 export class AppComponent { }
