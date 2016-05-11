655f61c8a735f44b134b2cf7d106388f60119c43
jayd <jayd@microsoft.com>
2016-05-10 21:36:27 -0700

    port more over

--- src/app/services/fiddle.service.ts
+++ src/app/services/fiddle.service.ts
@@ -11,25 +11,4 @@ export class FiddleService {
         return this.http.get("http://jsonplaceholder.typicode.com/posts")
                    .map((res:Response) => res.json());
     } 
-    
-    //original to port
-    // function getData() {
-    //     var url = "http://jsonplaceholder.typicode.com/posts";
-    //     return $resource(url).query();
-    // }
-    
-    
-    //Fake note
-    // getHero(id: number) {
-    //     return Promise.resolve(HEROES).then(
-    //         heroes => heroes.filter(hero => hero.id === id)[0]
-    //     );
-    // }
-    
-    //Fake timing
-    // getHeroesSlowly() {
-    // return new Promise<Hero[]>(resolve =>
-    //     setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
-    // );
-    // }
 }
